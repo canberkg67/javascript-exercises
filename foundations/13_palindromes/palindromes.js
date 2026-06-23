81571cf0a20a7e7f4b47c2b6c65ab1644d462b9a
Canberk <canberkg98@hotmail.com>
2026-06-23 14:26:46 +0300

    done palindrome function

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const cleaned = text.toLowerCase().replace(/[^a-z0-9]/g, "") //boşluk ve noktalamaları silmek için a-z ve 0-9 hariç herşey siliniyor.
+    const reversed = cleaned.split("").reverse().join("");
+    return cleaned === reversed;
+
+    //split ile text'i array'e çeviriyoruz.reverse metodu arrayde çalışıyor.
+    //array e çevirip tersini aldıktan sonra tekrar string'e birleştirmek için join diyoruz.
+    //split ve join'deki içi boş tırnaklar nereden kesilip birleşeceğini gösteriyor.
 
 };
-
 // Do not edit below this line
 module.exports = palindromes;

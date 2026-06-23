c5e1313cf83547fa20f1bee7e4db62b4377bedc8
Canberk <canberkg98@hotmail.com>
2026-06-23 13:41:54 +0300

    calculator practices are completed

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -2,25 +2,50 @@ const add = function(x,y) {
 	return x + y;
 };
 
+console.log(add(4,5));
+
 const subtract = function(a,b) {
 	return a - b;
 };
 
-const sum = function() {
-	
+console.log(subtract(5,2));
+
+const sum = function(array) {
+	let total = 0;
+  for (let i=0; i < array.length; i++){
+    total += array[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
+console.log(sum([1,2,3]));
 
+const multiply = function(array) {
+  let total = 1;
+  for(let i=0; i < array.length; i++){
+    total *= array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+console.log(multiply([2,4,6,8]));
+
+const power = function(a,b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+console.log(power(5,3));
+
+const factorial = function(number) {
+  let total = 1;
+  for (let i=number; i>0; i-- ){
+    total *= number;
+  }
+  return total;
 };
+
+console.log(factorial(3));
 
 // Do not edit below this line
 module.exports = {

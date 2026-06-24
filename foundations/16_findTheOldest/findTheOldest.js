a01addc54337ac810f9c96fc532bdfd71fe95b08
Canberk <canberkg98@hotmail.com>
2026-06-24 13:13:54 +0300

    find oldest practice is done

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest , person) => {
+        const getAge = (p) => {
+            const end = p.yearOfDeath || new Date().getFullYear();
+            return end - p.yearOfBirth ;
+        };
+        
+        return getAge(person) > getAge(oldest) ? person : oldest;
+        
+    }) //ilk adım yazmazsan default ilk eleman ilk adım oluyor.
 };
 
 // Do not edit below this line
